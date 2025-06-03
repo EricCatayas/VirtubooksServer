c20f16fe5d3560cbbceb2ef40c9648f9bdf56ac2
EricCatayas <catayasericjay@gmail.com>
2025-06-03 09:19:00 +0800

    update: merge pages route to notebook route

--- routes/notebookRoute.js
+++ routes/notebookRoute.js
@@ -2,6 +2,7 @@ const express = require("express");
 const router = express.Router();
 const { authMiddleware } = require("../middlewares/authMiddleware.js");
 const NotebookController = require("../controllers/notebookController.js");
+const PageController = require("../controllers/pageController.js");
 
 router.get("/", NotebookController.getPublicNotebooks);
 router.get("/user", authMiddleware, NotebookController.getUserNotebooks);
@@ -9,5 +10,7 @@ router.post("/", authMiddleware, NotebookController.createNotebook);
 router.get("/:id", authMiddleware, NotebookController.getNotebook);
 router.put("/:id", authMiddleware, NotebookController.updateNotebook);
 router.delete("/:id", authMiddleware, NotebookController.deleteNotebook);
+router.put("/:id/pages", authMiddleware, PageController.updatePages);
+router.put("/:id/pages/:pageId", authMiddleware, PageController.updatePages);
 
 module.exports = router;
