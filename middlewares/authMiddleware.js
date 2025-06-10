2abb84f20b3190fcdbcfb3c32a5a2c1847d5904e
EricCatayas <catayasericjay@gmail.com>
2025-06-10 15:28:18 +0800

    feat: parse user from token

--- middlewares/authMiddleware.js
+++ middlewares/authMiddleware.js
@@ -11,9 +11,24 @@ module.exports.authMiddleware = (req, res, next) => {
   try {
     const decoded = jwt.verify(token, process.env.ACCESS_TOKEN_SECRET);
     req.user = decoded;
-    console.log("User authenticated:", req.user, token);
     next();
   } catch (err) {
     return res.status(401).json({ error: "Invalid token" });
+  }
+};
+
+module.exports.parseUserFromToken = (req, res, next) => {
+  const authHeader = req.headers.authorization;
+  if (!authHeader || !authHeader.startsWith("Bearer ")) {
+    return next(); // No token provided, proceed without user info
+  }
+
+  const token = authHeader.split(" ")[1];
+  try {
+    const decoded = jwt.verify(token, process.env.ACCESS_TOKEN_SECRET);
+    req.user = decoded;
+    next();
+  } catch (err) {
+    next();
   }
 };
