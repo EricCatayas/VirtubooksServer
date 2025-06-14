1fe29e00e456a5beca516a283598ee62142f9dee
EricCatayas <catayasericjay@gmail.com>
2025-06-14 18:32:16 +0800

    feat: getUser in AuthService

--- services/authService.js
+++ services/authService.js
@@ -1,13 +1,15 @@
 class AuthService {
-  apiUrl = "";
+  WP_API_BASE_URL = "";
+  API_BASE_URL = "";
   constructor() {
-    this.apiUrl = `${process.env.WORDPRESS_API_BASE_URL}/wp-json/virtubooks/v1`;
+    this.WP_API_BASE_URL = `${process.env.WP_API_BASE_URL}/wp-json/virtubooks/v1`;
+    this.API_BASE_URL = process.env.API_BASE_URL;
   }
 
   async getUser(user) {
     const password = user.password;
 
-    const response = await fetch(`${this.apiUrl}/auth`, {
+    const response = await fetch(`${this.WP_API_BASE_URL}/auth`, {
       method: "POST",
       headers: {
         "Content-Type": "application/json",
@@ -21,6 +23,40 @@ class AuthService {
     const data = await response.json();
 
     return data.user;
+  }
+
+  async getUserById(id) {
+    const response = await fetch(`${this.WP_API_BASE_URL}/users/${id}`, {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json",
+        Origin: this.API_BASE_URL,
+      },
+    });
+
+    if (!response.ok) {
+      throw new Error("Failed to fetch user by ID: ", response);
+    }
+    const data = await response.json();
+
+    return data.user;
+  }
+
+  async getUsers() {
+    const response = await fetch(`${this.WP_API_BASE_URL}/users`, {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json",
+        Origin: this.API_BASE_URL,
+      },
+    });
+
+    if (!response.ok) {
+      throw new Error("Failed to fetch users: ", response);
+    }
+    const data = await response.json();
+
+    return data.users;
   }
 }
 
