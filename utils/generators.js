a40f5cd83187a5731d9e929974434df0bd209c20
EricCatayas <catayasericjay@gmail.com>
2025-06-18 10:17:31 +0800

    feat: generate slug util

--- utils/generators.js
+++ utils/generators.js
@@ -3,3 +3,12 @@ const { v4: uuidv4 } = require("uuid");
 module.exports.generateUID = () => {
   return uuidv4();
 };
+
+module.exports.generateSlug = (title) => {
+  return title
+    .toLowerCase()
+    .replace(/[^a-z0-9\s-]/g, "") // Remove non-alphanumeric characters except spaces and hyphens
+    .trim()
+    .replace(/\s+/g, "-") // Replace spaces with hyphens
+    .substring(0, 50); // Limit to 50 characters
+};
